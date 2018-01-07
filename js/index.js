f073a5e2ee6e99e00f2cd76cf8c2fd57ed6038c4
Stanislau <stanislau.s@yandex.ru>
2018-01-07 16:15:45 +0000

    commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function soundClickDuck() {
   var audio = new Audio();
   audio.src = 'audio/quac.mp3';
-  audio.volume = 0.2;
+  audio.volume = 1;
   audio.autoplay = true;
 }
 
@@ -15,13 +15,6 @@ function soundClickButton() {
 function soundClickBody() {
   var audio = new Audio();
   audio.src = 'audio/shoot.mp3';
-  audio.volume = 0.1;
-  audio.autoplay = true;
-}
-
-function onloadClap() {
-  var audio = new Audio();
-  audio.src = 'audio/clap.mp3';
   audio.volume = 0.1;
   audio.autoplay = true;
 }
