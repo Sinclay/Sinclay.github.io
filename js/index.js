0f268761005803b68755ff10fe2c461b71408956
Stanislau <stanislau.s@yandex.ru>
2018-01-09 19:58:35 +0000

    commit

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ function soundClickBody() {
   audio.volume = 0.1;
   audio.autoplay = true;
 }
+
+function checkClick() {
+  document.getElementById('bonusDuck').checked = false;
+  document.getElementById('duck1').checked = false;
+  document.getElementById('duck2').checked = false;
+  document.getElementById('duck3').checked = false;
+  document.getElementById('duck4').checked = false;
+  document.getElementById('duck5').checked = false;
+  document.getElementById('duck6').checked = false;
+  document.getElementById('duck7').checked = false;
+  document.getElementById('duck8').checked = false;
+  document.getElementById('duck9').checked = false;
+  document.getElementById('duck10').checked = false;
+  document.getElementById('duck11').checked = false;
+  document.getElementById('duck12').checked = false;
+  document.getElementById('duck13').checked = false;
+  document.getElementById('duck14').checked = false;
+  document.getElementById('duck15').checked = false;
+}
